1f798510c352419f26d360034399744f4a3be4ac
Gilmer Contreras <ivsmca90@gmail.com>
2025-05-12 22:16:39 -0400

    up

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ require('dotenv').config();
 const path = require('path');
 
 const express = require('express');
-const robots = require('express-robots-txt')
+
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const robots = require('express-robots-txt'); 
 
 /*
 ? INIT EXPRESS SERVER
@@ -24,8 +25,15 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
-// ROBOTS
-app.use(robots(__dirname + '/robots.txt'));
+// ⬇️ Middleware para robots.txt
+app.use(
+    robots({
+      UserAgent: '*',
+      Disallow: ['/admin/', '/api/', '/cloud/'],
+      Allow: ['/assets/', '/'],
+      Sitemap: 'https://rifari.com/assets/sitemap.xml',
+    })
+  );
 
 /* 
 ? DIRECTORIO PUBLICO
